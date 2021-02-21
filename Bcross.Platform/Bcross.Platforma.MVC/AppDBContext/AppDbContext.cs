4af096f34df535ecf594e56870845bf9f562d88b
Hamlet Poghosyan <hamlet.poghosyan@volo.global>
2021-02-21 10:56:14 +0400

    add db context

---
+++ Bcross.Platform/Bcross.Platforma.MVC/AppDBContext/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bcross.Platforma.MVC.Models.AppDBContext
+{
+    public class AppDbContext:DbContext
+    {
+
+    }
+}
