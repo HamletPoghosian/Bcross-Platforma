6710cfa4adc8c4f90ef1ec60889083c9eec324eb
Hamlet Poghosyan <hamlet.poghosyan@volo.global>
2021-02-21 15:42:28 +0400

    implementation of  CompanyRepository  add  5 new methods . get full functionality

--- Bcross.Platform/Bcross.Platforma.MVC/Data/Repositories/Extensions/CompanyRepository.cs
+++ Bcross.Platform/Bcross.Platforma.MVC/Data/Repositories/Extensions/CompanyRepository.cs
@@ -23,5 +23,41 @@ namespace Bcross.Platforma.MVC.Data.Repositories
             var compies = await _context.Company.ToListAsync();
             return compies;
         }
+
+        public async Task<Company> GetCompanyById(long id)
+        {
+            if (id == 0)
+            {
+                return null;
+            }
+
+           var company = await _context.Company.Where(c => c.Id == id).FirstOrDefaultAsync();
+
+            return company;
+        }
+
+        public async Task<List<Company>> GetCompanyByIds(List<long> companyIds)
+        {
+            if (companyIds.Count== 0)
+            {
+                return null;
+            }
+
+            var company = await _context.Company.Where(c => companyIds.Contains(c.Id)).ToListAsync();
+
+            return company.Count == 0 ? null : company;
+        }
+
+        public async Task<Company> GetCompanyByName(string companyName)
+        {
+            if (string.IsNullOrEmpty(companyName))
+            {
+                return null;
+            }
+
+            var company = await _context.Company.Where(c => c.Name.Contains(companyName)).FirstOrDefaultAsync();
+
+            return company;
+        }
     }
 }
