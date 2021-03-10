eec8d8b3c1e260a560f6859df947c1f11f3b5a7b
Hamlet Poghosyan <hamlet.poghosyan@volo.global>
2021-03-10 09:57:50 +0400

    (BSS-007 )  add ContractService  and inteface

---
+++ Bcross.Platform/Bcross.Platforma.MVC/Data/Services/Extensions/ContractService.cs
@@ -0,0 +1,37 @@
+﻿using Bcross.Platforma.MVC.Data.Services.Interfaces;
+using Bcross.Platforma.MVC.Models.DataTransferObjects.Companies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bcross.Platforma.MVC.Data.Services.Extensions
+{
+    public class ContractService : IContractService
+    {
+        public Task<ContractDTO> CreateContractAsync(ContractDTO company)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ContractDTO>> GetAllContractsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ContractDTO>> GetContractByFirstCompanyIdsAsync(List<long> companyIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ContractDTO> GetContractByIdAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ContractDTO>> GetContractBySecoundCompanyIdsAsync(List<long> companyIds)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
