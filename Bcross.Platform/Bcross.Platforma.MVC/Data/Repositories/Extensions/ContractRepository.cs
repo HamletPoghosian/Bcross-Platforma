aa5e95400c771fc641ea887e3b52925491a23700
Hamlet Poghosyan <hamlet.poghosyan@volo.global>
2021-03-10 09:48:50 +0400

    add  ContractRepository

---
+++ Bcross.Platform/Bcross.Platforma.MVC/Data/Repositories/Extensions/ContractRepository.cs
@@ -0,0 +1,37 @@
+﻿using Bcross.Platforma.MVC.Data.Repositories.Interfaces;
+using Bcross.Platforma.MVC.Models.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bcross.Platforma.MVC.Data.Repositories.Extensions
+{
+    public class ContractRepository : IContractRepository
+    {
+        public Task<Contract> CreateContract(Contract company)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Contract>> GetAllContracts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Contract>> GetContractByFirstCompanyIds(List<long> companyIds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Contract> GetContractById(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Contract>> GetContractBySecoundCompanyIds(List<long> companyIds)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
