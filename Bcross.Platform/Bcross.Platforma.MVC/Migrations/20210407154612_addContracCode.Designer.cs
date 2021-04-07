7bd9f9e2c02b1422ff9a5fcf58620125caba40f5
Hamlet Poghosyan <hamlet.poghosyan@volo.global>
2021-04-07 19:46:38 +0400

    add new column in DB Contract table

--- Bcross.Platform/Bcross.Platforma.MVC/Migrations/20210407154612_addContracCode.Designer.cs
+++ Bcross.Platform/Bcross.Platforma.MVC/Migrations/20210407154612_addContracCode.Designer.cs
@@ -4,14 +4,16 @@ using Bcross.Platforma.MVC.Models.AppDBContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Bcross.Platforma.MVC.Migrations
 {
     [DbContext(typeof(BcrossContext))]
-    partial class BcrossContextModelSnapshot : ModelSnapshot
+    [Migration("20210407154612_addContracCode")]
+    partial class addContracCode
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
