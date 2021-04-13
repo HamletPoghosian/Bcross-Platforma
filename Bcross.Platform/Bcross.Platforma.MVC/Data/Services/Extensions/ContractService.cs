d34e94e96b3717076f1dd5706b0e95b8b13f6eb4
Hamlet Poghosyan <hamlet.poghosyan@volo.global>
2021-04-13 10:17:46 +0400

    add GetAllContractsAsync in  IContractService

--- Bcross.Platform/Bcross.Platforma.MVC/Data/Services/Extensions/ContractService.cs
+++ Bcross.Platform/Bcross.Platforma.MVC/Data/Services/Extensions/ContractService.cs
@@ -1,4 +1,5 @@
-﻿using Bcross.Platforma.MVC.Data.Repositories.Interfaces;
+﻿using Bcross.Platforma.MVC.Data.Mappers;
+using Bcross.Platforma.MVC.Data.Repositories.Interfaces;
 using Bcross.Platforma.MVC.Data.Services.Interfaces;
 using Bcross.Platforma.MVC.Models.DataTransferObjects.Companies;
 using System;
@@ -11,10 +12,12 @@ namespace Bcross.Platforma.MVC.Data.Services.Extensions
     public class ContractService : IContractService
     {
         private readonly IContractRepository _contractRepository;
+        private readonly ICompanyMapper _companyMapper;
 
-        public ContractService(IContractRepository contractRepository)
+        public ContractService(IContractRepository contractRepository, ICompanyMapper companyMapper)
         {
             _contractRepository = contractRepository;
+            _companyMapper = companyMapper;
         }
 
         public async Task<ContractDTO> CreateContractAsync(ContractDTO company)
@@ -22,8 +25,11 @@ namespace Bcross.Platforma.MVC.Data.Services.Extensions
             throw new NotImplementedException();
         }
 
-        public Task<List<ContractDTO>> GetAllContractsAsync()
+        public async Task<List<ContractDTO>> GetAllContractsAsync()
         {
+            var contracts = await _contractRepository.GetAllContracts();
+            var contractsDTO = _companyMapper.ToContractDTO(contracts);
+            return contractsDTO;
         }
 
         public Task<List<ContractDTO>> GetContractByFirstCompanyIdsAsync(List<long> companyIds)
